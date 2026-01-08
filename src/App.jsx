f24724e07cd5440d815da69665507c10856a1df0
rayliu7717 <rayliu2004@gmail.com>
2026-01-07 21:49:10 -0500

    Integrating JavaScript in JSX with Curly Braces

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
 const App = () => {
-  return <h1>React is fun!</h1>;
+  const userName = "Alice";
+
+  const greetUser = (name) => `Hello, ${name}!`;
+
+  const userInfo = { age: 25, location: "New York" };
+
+  // Inline style object
+  const titleStyle = {
+    color: "blue",
+    fontSize: "24px",
+    textAlign: "center",
+    margin: "20px 0",
+  };
+
+  return (
+    <div>
+      {/* Passing strings with quotes */}
+      <img src='images/nature.jpg' alt='Nature' width='300' />
+
+      {/* Referencing a JavaScript variable */}
+      <p>Your name is: {userName}</p>
+
+      {/* Calling a JavaScript function */}
+      <p>Greeting: {greetUser(userName)}</p>
+
+      {/* Using a JavaScript object */}
+      <p>
+        Age: {userInfo.age}, Location: {userInfo.location}
+      </p>
+
+      <h1 style={titleStyle}>Welcome to React!</h1>
+    </div>
+  );
 };
 
 // Export the App component
