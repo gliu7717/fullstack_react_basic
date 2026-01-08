dcc0f24c483175658f78fa05118c9b080436b214
rayliu7717 <rayliu2004@gmail.com>
2026-01-07 21:57:28 -0500

    Styling React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
+import GoogleComponent from "./components/GoogleComponent";
+import AppleComponent from "./components/AppleComponent";
+import AmazonComponent from "./components/AmazonComponent";
+
 const App = () => {
-  const userName = "Alice";
-
-  const greetUser = (name) => `Hello, ${name}!`;
-
-  const userInfo = { age: 25, location: "New York" };
-
-  // Inline style object
-  const titleStyle = {
-    color: "blue",
-    fontSize: "24px",
-    textAlign: "center",
-    margin: "20px 0",
-  };
-
   return (
     <div>
-      {/* Passing strings with quotes */}
-      <img src='images/nature.jpg' alt='Nature' width='300' />
-
-      {/* Referencing a JavaScript variable */}
-      <p>Your name is: {userName}</p>
-
-      {/* Calling a JavaScript function */}
-      <p>Greeting: {greetUser(userName)}</p>
-
-      {/* Using a JavaScript object */}
-      <p>
-        Age: {userInfo.age}, Location: {userInfo.location}
-      </p>
-
-      <h1 style={titleStyle}>Welcome to React!</h1>
+      <GoogleComponent />
+      <AppleComponent />
+      <AmazonComponent />
     </div>
   );
 };
